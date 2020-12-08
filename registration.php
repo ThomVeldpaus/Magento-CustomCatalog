e1ef7ef9c99d36fcd412e89f95bd345cbdc5e146
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2020-12-08 12:33:04 +0100

    Try to fix cataloge product indexer problems

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Module registration file
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Thom_CustomCatalog', __DIR__);
